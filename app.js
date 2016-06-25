b77281ea7a86fe883030ab519cc152dd6b36676c
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-06-25 04:23:07 -0600

    Added article provider

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 // Module dependencies.
 var express = require('express');
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
 var app = express();
 
-// Configuration
-//app.configure( function() {
-//});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
 
-// Routes
-app.get('/', function(req, res) {
-    res.send('Hello World');
-});
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+/*app.configure('production', function(){
+  app.use(express.errorHandler());
+});*/
+
+var articleProvider= new ArticleProvider();
+
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.send(docs);
+  });
+})
 
 app.listen(3000);
