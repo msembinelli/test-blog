f1123343d469d3c6cdaa5656e337895c8842c9df
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-06-27 22:24:15 -0600

    Update methodOverride call to express 4.x

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
-app.use(express.methodOverride());
+app.use(methodOverride());
 app.use(require('stylus').middleware({ src: __dirname + '/public' }));
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
