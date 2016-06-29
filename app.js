44e61a61fefd63f00ca7a0952a7015e9fcdd28c3
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-06-29 01:56:15 -0600

    Added skeleton post/gets for passport

--- app.js
+++ app.js
@@ -8,14 +8,54 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
 
+//Auth passport.js stuff
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+
+//We will be creating these two files shortly
+// var config = require('./config.js'), //config file contains all tokens and other private info
+//    funct = require('./functions.js'); //funct file contains our helper functions for our Passport and database work
+
+//===============PASSPORT===============
+
+//This section will contain our work with Passport
+
+//===============EXPRESS================
+app.use(logger('combined'));
+app.use(cookieParser());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(require('stylus').middleware({ src: __dirname + '/public' }));
 
+// Session-persisted message middleware
+app.use(function(req, res, next){
+  var err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
 var articleProvider= new ArticleProvider('localhost', 27017);
+
+//===============ROUTES===============
 
 app.get('/', function(req, res){
     articleProvider.findAll( function(error,docs){
@@ -24,6 +64,30 @@ app.get('/', function(req, res){
             articles:docs
         });
     })
+});
+
+//displays our signup page
+app.get('/admin', function(req, res){
+  res.render('admin.jade', { locals:{
+    title: 'Sign In'
+  }
+});
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', {
+  successRedirect: '/blog/new',
+  failureRedirect: '/admin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
 });
 
 app.get('/blog/new', function(req, res) {
@@ -64,5 +128,7 @@ app.post('/blog/addComment', function(req, res) {
 
 app.use(express.static(__dirname + '/public'));
 app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+
+//===============PORT=================
 
 app.listen(3000);
