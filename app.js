cca6450037698f22fc19d841c8c53aa67f8654dc
Matthew Sembinelli <matthew.sembinelli@gmail.com>
2016-06-29 19:10:48 -0600

    Basic local strategy implemented for new blog posts

--- app.js
+++ app.js
@@ -7,21 +7,18 @@ var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
-
-//Auth passport.js stuff
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
-
-//We will be creating these two files shortly
-// var config = require('./config.js'), //config file contains all tokens and other private info
-//    funct = require('./functions.js'); //funct file contains our helper functions for our Passport and database work
 
 //===============PASSPORT===============
-
-//This section will contain our work with Passport
+var dbConfig = require('./config/db.js');
+var mongoose = require('mongoose');
+mongoose.connect(dbConfig.url);
+var passport = require('passport');
+var session = require('express-session');
+app.use(session({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //===============EXPRESS================
 app.use(logger('combined'));
@@ -30,28 +27,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(methodOverride());
 app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-
-// Session-persisted message middleware
-app.use(function(req, res, next){
-  var err = req.session.error,
-      msg = req.session.notice,
-      success = req.session.success;
-
-  delete req.session.error;
-  delete req.session.success;
-  delete req.session.notice;
-
-  if (err) res.locals.error = err;
-  if (msg) res.locals.notice = msg;
-  if (success) res.locals.success = success;
-
-  next();
-});
 
 var articleProvider= new ArticleProvider('localhost', 27017);
 
@@ -66,36 +43,12 @@ app.get('/', function(req, res){
     })
 });
 
-//displays our signup page
-app.get('/admin', function(req, res){
-  res.render('admin.jade', { locals:{
-    title: 'Sign In'
-  }
-});
-});
-
-//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
-app.post('/login', passport.authenticate('local-signin', {
-  successRedirect: '/blog/new',
-  failureRedirect: '/admin'
-  })
-);
-
-//logs user out of site, deleting them from the session, and returns to homepage
-app.get('/logout', function(req, res){
-  var name = req.user.username;
-  console.log("LOGGIN OUT " + req.user.username)
-  req.logout();
-  res.redirect('/');
-  req.session.notice = "You have successfully been logged out " + name + "!";
-});
-
-app.get('/blog/new', function(req, res) {
+/*app.get('/blog/new', function(req, res) {
     res.render('blog_new.jade', { locals: {
         title: 'New Post'
         }
     });
-});
+});*/
 
 app.post('/blog/new', function(req, res){
      articleProvider.save({
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
+
+module.exports = function(passport){
+
+  /* GET login page. */
+  router.get('/admin', function(req, res) {
+    // Display the Login page with any flash message, if any
+    res.render('admin.jade', { message: req.flash('message') });
+  });
+
+  /* Handle Login POST */
+  router.post('/login', passport.authenticate('login', {
+    successRedirect: '/blog/new',
+    failureRedirect: '/admin',
+    failureFlash : true
+  }));
+
+  /* GET Registration Page */
+  router.get('/signup', function(req, res){
+    res.render('register.jade',{message: req.flash('message')});
+  });
+
+  /* Handle Registration POST */
+  router.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/blog/new',
+    failureRedirect: '/signup',
+    failureFlash : true
+  }));
+
+  /* GET Home Page */
+router.get('/blog/new', isAuthenticated, function(req, res){
+  res.render('blog_new.jade', { user: req.user });
 });
 
-module.exports = router;
+/* Handle Logout */
+router.get('/signout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+  return router;
+}
